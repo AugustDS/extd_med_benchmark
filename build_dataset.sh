e351028f248294f785fe7edd0fe80101a5f539cc
augustd <augustd@student.ethz.ch>
2021-03-02 14:14:31 +0100

    1

---
+++ build_dataset.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -l 
+
+# Brain Dataset
+python3 /build_dataset/dataset_tool.py create_from_brain --tfrecord_dir /scratch/aschuette/ct_scans_brain --image_dir /scratch/aschuette/brain_dataset/resolution
