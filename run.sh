d4692a07095405367b361b85c3af03868e2f5d40
augustd <augustd@student.ethz.ch>
2021-03-04 12:08:19 +0100

    .

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module load cuda/10.0
 module load cudnn/7.5-cu10.0
-/work/aschuette/miniconda3/bin/python3 -Xfaulthandler /home/aschuette/extd_med_benchmark/GAN_cpd/train.py train --data_dir /scratch/aschuette/brain_dataset/resolution --results_dir /work/aschuette/result_test_hr/1.0/000 --random_sed 1000 --resolution 256 --num_gpus 8 --learn_rate 0.005 --batch_size 256 --disc_repeats 1 --total_kimg 20
+/work/aschuette/miniconda3/bin/python3 -Xfaulthandler /home/aschuette/extd_med_benchmark/GAN_cpd/train.py train /scratch/aschuette/brain_dataset/resolution /work/aschuette/result_test_hr/1.0/000 1000 256 8 0.005 256 1 20
