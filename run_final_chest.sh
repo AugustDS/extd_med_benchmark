6ff7f5583938e7ae524a88d52331ac2b5f12f646
augustd <augustd@student.ethz.ch>
2021-03-23 17:32:02 +0100

    run_files

--- run_final_chest.sh
+++ run_final_chest.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -l 
 # /train.py train data_dir result_dir load_network_run_id resume_kimgs random_seed resolution num_gpus learn_rate batch_size d_repeats total_kimg
 
-# Brain CT scans
-# 256^2: Restart from RUN 1.0/002 (FID 10.17)
-python3 GAN_cpd/train.py train $2 $1/1.0/002 $1/1.0/002/network-final.pkl 4000 1000 256 8 0.005 160 1 12000
-# 512^2: Restart from RUN 2.0/001 (FID 15.71)
-python3 GAN_cpd/train.py train $2 $1/2.0/001 $1/2.0/001/network-final.pkl 4000 1000 512 8 0.005 64 1 12000
-
 # Chest X-Rays
 # 256^2: Restart from RUN 3.0/002 (FID 31.07)
 python3 GAN_cpd/train.py train $2 $1/3.0/002 $1/3.0/002/network-final.pkl 4000 1000 256 8 0.005 160 1 12000
