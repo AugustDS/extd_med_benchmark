9cd8d601f94a37dcba78b100eedfcf03e20f6bc3
augustd <augustd@student.ethz.ch>
2021-05-17 09:54:05 +0200

    trans cehst

--- transform_nn_cxpl.py
+++ transform_nn_cxpl.py
@@ -4,9 +4,9 @@ import PIL
 from PIL import Image
 import pandas as pd
 
-base_path = "/work/aschuette/extd_med_benchmark/results/1.0/002/classification_results"
+base_path = "/work/aschuette/extd_med_benchmark/results/3.0/002/classification_results"
 resolution = 256
-chest = False
+chest = True
 
 if resolution == 512:
     directory_224 = base_path + "/224/nn_files" 
